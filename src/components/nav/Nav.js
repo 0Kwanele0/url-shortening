175a68f70e2a1c96d583618f5ac3264811b69005
kwanele <kwanelegamedze4@gmail.com>
2021-08-27 23:47:29 +0200

    made the bav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,12 +1,19 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useState} from 'react'
 import './styles/nav.css'
 
 function Nav() {
+
+    let menu = useRef()
+
+    function changer() {
+        menu.current.style.display == "flex" ? menu.current.style.display = "none" : menu.current.style.display = "flex"
+    }
     
     return (
         <div className="nav">
             <h1 className="nav__logo">Shortly</h1>
-            <div className="nav__list">
+           
+            <div ref={menu} className="nav__list">
                 <ul>
                     <li>Features</li>
                     <li>Resources</li>
@@ -17,7 +24,7 @@ function Nav() {
                     <button>Sign Up</button>
                 </div>
             </div>
-            <img className="nav__icon" src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png" alt="menu"/>
+            <img onClick={changer} className="nav__icon" src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png" alt="menu"/>
         </div>
     )
 }
