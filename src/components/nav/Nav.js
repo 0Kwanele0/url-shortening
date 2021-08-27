fd29f4f18131d3d3bdd2d73a546be8695486e998
kwanele <kwanelegamedze4@gmail.com>
2021-08-27 21:19:23 +0200

    made the nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,7 +4,19 @@ import './styles/nav.css'
 function Nav() {
     return (
         <div className="nav">
-            <h1>Navaaa</h1>
+            <h1 className="nav__logo">Shortly</h1>
+            <div className="nav__list">
+                <ul>
+                    <li>Features</li>
+                    <li>Resources</li>
+                    <li>Pricing</li>
+                </ul>
+                <div className="btns">
+                    <button>Login</button>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+            <img className="nav__icon" src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png" alt="menu"/>
         </div>
     )
 }
