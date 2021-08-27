af4a46837ec86fcfd4a05f34071214e90ad00e2a
kwanele <kwanelegamedze4@gmail.com>
2021-08-27 18:44:09 +0200

    replaced placeholder content with real content

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './styles/statistics.css'
 import branded from '../../images/icon-brand-recognition.svg'
+import records from '../../images/icon-detailed-records.svg'
+import custo from '../../images/icon-fully-customizable.svg'
 
 function Statistics() {
     return (
@@ -17,18 +19,16 @@ function Statistics() {
                         help instill confidance in your content</p>
                 </div>
                 <div className="indi">
-                    <div className="img-cont"><img src={branded} alt="icon" /></div>
-                    <h3>Brand Recognition</h3>
-                    <p>Boost your brand recopgnition with each click.
-                        Generic links don't mean a thing. Branded links
-                        help instill confidance in your content</p>
+                    <div className="img-cont"><img src={records} alt="icon" /></div>
+                    <h3>Detailed Records</h3>
+                    <p>Get insight into who is clicking your links.
+                        Knowing where and when people engage with your content helps inform better decision.
+                    </p>
                 </div>
                 <div className="indi">
-                    <div className="img-cont"><img src={branded} alt="icon" /></div>
-                    <h3>Brand Recognition</h3>
-                    <p>Boost your brand recopgnition with each click.
-                        Generic links don't mean a thing. Branded links
-                        help instill confidance in your content</p>
+                    <div className="img-cont"><img src={custo} alt="icon" /></div>
+                    <h3>Fully Customizable</h3>
+                    <p>Improve brand awareness and content discovarability through customizable links. Supercharging audiance engagement.</p>
                 </div>
             </div>
         </div>
