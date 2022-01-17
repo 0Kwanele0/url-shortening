cd2b9933231dcef7a482b91abecded386bc6c818
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-01-17 13:53:24 +0200

    fixed color state management

--- src/components/ShowLink/ShowLink.js
+++ src/components/ShowLink/ShowLink.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import './styles/showlink.css'
+import {useState} from 'react'
+import './styles/showlink.scss'
 
 function ShowLink(props) {
+    const [btnText, setBtnText] = useState("Copy")
+    const [btncolor, setBtncolor] = useState("cyan")
+
+    function resetBtnText(){
+        setTimeout(() => {
+            setBtnText("Copy")
+            setBtncolor("cyan")
+        }, 2000);
+    }
+    
     return (
         <div className="show">
             {props.list &&
@@ -11,11 +21,14 @@ function ShowLink(props) {
                     <div key={key} className="show__indi">
                             <h5 className="long-link">{item.original_link}</h5>
                             <h5 className="short-link">{ item.short_link}</h5>
-                            <button onClick={(e) => {
+                            <button style={{backgroundColor: btncolor }} onClick={(e) => {
                                 e.preventDefault()
                                 navigator.clipboard.writeText(item.short_link)
+                                setBtnText("Copied!")
+                                setBtncolor("hsl(257, 27%, 26%)")
+                                resetBtnText()
                             }
-                        }>Copy</button>
+                        }>{btnText}</button>
                     </div>
                     )
                 })
